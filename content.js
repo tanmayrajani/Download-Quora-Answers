54f8bb7e2984d839a8b985ccc70a6988499cef41
Tanmay <tanmay.rajani@ishisystems.com>
2018-04-01 12:09:58 +0530

    fix changes in DOM and styles

--- content.js
+++ content.js
@@ -3,16 +3,16 @@ $(document).ready(function() {
 	var but = url.split('/');
 	var main_div, download_button, db;
 	if(but[but.length - 2] == 'answer' || but[but.length - 2] == 'answers'){
-		main_div = $('.Answer.ActionBar > .action_bar_inner')[0];
+		main_div = $('.Answer .action_bar_inner')[0];
 	}
 	else if(but[but.length - 2] == 'Posts' || (but[2]!='www.quora.com' && but[3]!='')){
-		main_div = $('.Post.ActionBar > .action_bar_inner')[0];
+		main_div = $('.BoardItem .action_bar_inner')[0];
 	}
-	download_button = document.createElement('div');
-	download_button.setAttribute("class", "action_item");
+	download_button = document.createElement('span');
+	download_button.setAttribute("id", "dqa-download-button");
 	db = document.createElement('a');
-	db.setAttribute("id", "download");
-	db.style.color = "rgb(153,153,153)";
+	db.setAttribute("class", "icon_action_bar-button");
+	db.style.color = "#666";
 	db.href = "#";
 	db.innerHTML = "Download";
 	download_button.appendChild(db);
